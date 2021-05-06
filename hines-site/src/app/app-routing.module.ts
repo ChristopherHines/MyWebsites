2c3cf6c0865f6a8ea8f8adf8e6314c377dc2889d
Christopher Hines <christopherhines2016@gmail.com>
2021-05-06 17:04:03 -0500

    added routing to project

--- hines-site/src/app/app-routing.module.ts
+++ hines-site/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BillTrackerComponent } from './bill-tracker/bill-tracker.component';
+import { PiholeComponent } from './pihole/pihole.component';
+import { PivpnComponent } from './pivpn/pivpn.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [ { path: '', redirectTo: 'home', pathMatch: 'full' },
+                         { path: 'home', component: HomeComponent},
+                         { path: 'bill-tracker', component: BillTrackerComponent },
+                         { path: 'pihole', component: PiholeComponent },
+                         { path: 'pivpn', component: PivpnComponent }
+                       ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
