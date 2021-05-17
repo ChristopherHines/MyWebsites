62badc0ba2417601bbd7832613b1e6c70770533e
Christopher Hines <christopherhines2016@gmail.com>
2021-05-17 06:43:19 -0500

    got pihole-walkthrough branch to mvp, opening pull request soon

--- hines-site/src/app/pihole/pihole.component.ts
+++ hines-site/src/app/pihole/pihole.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pihole',
-  templateUrl: './pihole.component.html',
-  styleUrls: ['./pihole.component.css']
+  selector: "app-pihole",
+  templateUrl: "./pihole.component.html",
+  styleUrls: ["./pihole.component.css"],
 })
-
 export class PiholeComponent implements OnInit {
-  pihole_walkthrough = 'To start, install pihole to your raspberry pi.'
+  pihole_walkthrough = "To start, install pihole to your raspberry pi.";
 
-  constructor() { }
+  constructor() {}
   ngOnInit(): void {}
 
   scrollToTop() {
