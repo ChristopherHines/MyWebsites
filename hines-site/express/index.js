9b0fb77125363f89545b250941c0823d10db52d5
Christopher Hines <christopherhines2016@gmail.com>
2021-07-03 13:30:03 -0500

    trying to add self signed cert

--- hines-site/express/index.js
+++ hines-site/express/index.js
@@ -1,15 +1,38 @@
-var express = require('express');
-var PORT = 80;
+// var express = require('express');
+// var PORT = 80;
+
+// var app = express();
+
+// path = __dirname+'/dist';
+// app.use(express.static(path));
+
+// app.get('/', function (req, res) {
+// - res.sendFile('dist/index.html');
+// });
+
+// app.listen(PORT);
+
+// console.log('Running on http://localhost:' + PORT);
+
+var fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    express = require('express');
+
+var port = 80;
+
+var options = {
+    key: fs.readFileSync('./10275765_http192.168.1.28.key'),
+    cert: fs.readFileSync('./10275765_http192.168.1.28.cert'),
+};
 
 var app = express();
 
-path = __dirname+'/dist';
-app.use(express.static(path));
-
-app.get('/', function (req, res) {
-- res.sendFile('dist/index.html');
+var server = https.createServer(options, app).listen(port, function(){
+  console.log("Express server listening on port " + port);
 });
 
-app.listen(PORT);
-
-console.log('Running on http://localhost:' + PORT);
+app.get('/', function (req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+});
